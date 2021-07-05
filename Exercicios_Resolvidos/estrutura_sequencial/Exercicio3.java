4db86bf05ef9c0e93cc37bef4a5704685ca33a2c
gustavohmacedo <gustavohm2008@hotmail.com>
2021-07-05 19:11:45 -0300

    Atualizando pacotes e adicionando exercicio 7

---
+++ Exercicios_Resolvidos/estrutura_sequencial/Exercicio3.java
@@ -0,0 +1,32 @@
+package exercicios_resolvidos.estrutura_sequencial;
+
+import java.util.Scanner;
+import java.util.Locale;
+
+public class Exercicio3 {
+    public static void main(String[] args) {
+
+        /*
+         * EXERCÍCIO 3: Faça um programa para ler o valor do raio de um círculo, e
+         * depois mostrar o valor da área deste círculo com quatro casas decimais.
+         * Fórmula da área: Área = (π . raio²), onde o valor de π = 3.14159
+         */
+
+        final double PI = 3.14159;
+        double raioDoCiculo;
+        double areaDoCirculo;
+
+        Locale.setDefault(Locale.US);
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Informe o raio do Círculo: ");
+        raioDoCiculo = scanner.nextDouble();
+
+        areaDoCirculo = PI * (Math.pow(raioDoCiculo, 2));
+
+        System.out.printf("Área = %.4f ", areaDoCirculo);
+
+        scanner.close();
+
+    }
+}
---
+++ Exercicios_Resolvidos/estrutura_sequencial/Exercicio5.java
@@ -0,0 +1,39 @@
+package exercicios_resolvidos.estrutura_sequencial;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+public class Exercicio5 {
+    public static void main(String[] args) {
+        /*
+         * Fazer um programa que leia o número de um funcionário, a quantidade de horas
+         * trabalhadas no mês, o valor que recebe por hora e calcular o salário desse
+         * funcionário. A seguir, mostre o número e o salário do funcionário, com duas
+         * casas decimais.
+         */
+
+        int numeroDoFuncionario = 0;
+        int qtdDeHorasTrabalhadas = 0;
+        double valorHoraDeTrabalho = 0.0;
+
+        Locale.setDefault(Locale.US);
+        Scanner leitor = new Scanner(System.in);
+
+        System.out.println("Funcionario, Informe o seu numero, por gentileza: ");
+        numeroDoFuncionario = leitor.nextInt();
+
+        System.out.println("Funcionario, Informe a quantidade de horas trabalhadas, por gentileza: ");
+        qtdDeHorasTrabalhadas = leitor.nextInt();
+
+        System.out.println("Funcionario, informe o valor da sua hora de trabalho , por gentileza: ");
+        valorHoraDeTrabalho = leitor.nextDouble();
+
+        System.out.println("\nO numero informado foi: " + numeroDoFuncionario);
+
+        double salarioTotalFuncionario = qtdDeHorasTrabalhadas * valorHoraDeTrabalho;
+        System.out.printf("O salario total é R$ %.2f \n", salarioTotalFuncionario);
+
+        leitor.close();
+    }
+
+}
